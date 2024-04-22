cbdc22f8f1681a4cdc0151105985ede7d1b2e1f0
HIMANSHU NEGI <himanxunegi@gmail.com>
2024-04-22 13:11:35 +0530

    Add components

---
+++ src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar"; // Import Navbar component
+import Footer from "../components/Footer"; // Import Footer component
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>Body</div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}

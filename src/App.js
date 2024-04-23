9d8bc5af6d47c4440c99e36bb0223858cd0a76a0
HIMANSHU NEGI <himanxunegi@gmail.com>
2024-04-23 17:32:36 +0530

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+
 function App() {
   return (
-    <>
+    <Router>
       <div>
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />}></Route>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar"; // Import Navbar component
 import Footer from "../components/Footer"; // Import Footer component
+
 export default function Home() {
   return (
     <div>

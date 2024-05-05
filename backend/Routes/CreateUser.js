5d313aa44e45f5420f74fe0d43611a80b2075b15
HIMANSHU NEGI <himanxunegi@gmail.com>
2024-05-05 21:55:50 +0530

    Add express-validator

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createUser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      location: req.body.location,
-    });
-    res.json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false });
+router.post(
+  "/createUser",
+  [
+    body("email").isEmail(),
+
+    body("name").isLength({ min: 5 }),
+    body("password", "Incorrect Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.json({ success: false });
+    }
   }
-});
+);
 module.exports = router;
